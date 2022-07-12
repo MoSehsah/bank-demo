bcf06ecf39ee746fa436a5016cbe6245b30b816f
Gorkem Ozlu <spammailatmayin@gmail.com>
2022-07-12 15:04:24 +0300

    add httpTrace

--- account-service/src/main/java/com/vmware/tanzu/account/AccountsApplication.java
+++ account-service/src/main/java/com/vmware/tanzu/account/AccountsApplication.java
@@ -2,11 +2,18 @@ package com.vmware.tanzu.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 
 @SpringBootApplication
 public class AccountsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountsApplication.class, args);
+    }
+    @Bean
+    public HttpTraceRepository htttpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
     }
 }
