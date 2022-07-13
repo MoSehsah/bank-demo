93757a572fe766286aa5315cbc8ffdb6ff51b9f1
Gorkem Ozlu <spammailatmayin@gmail.com>
2022-07-13 15:41:19 +0300

    big bang discovery-service

---
+++ discovery-server/src/main/java/com/vmware/tanzu/discovery/DiscoveryApplication.java
@@ -0,0 +1,21 @@
+package com.vmware.tanzu.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+      }
+    @Bean
+    public HttpTraceRepository htttpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
+}
