78ad132c5bc11193bf30c9cbf827d432ea5715e5
Gorkem Ozlu <spammailatmayin@gmail.com>
2022-07-14 20:26:57 +0300

    fix livez readyz

--- discovery-server/src/main/java/com/vmware/tanzu/discovery/SecurityConfig.java
+++ discovery-server/src/main/java/com/vmware/tanzu/discovery/SecurityConfig.java
@@ -11,8 +11,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
-        http.authorizeRequests().anyRequest().permitAll();
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+        
     }
 
-}
+}//http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
