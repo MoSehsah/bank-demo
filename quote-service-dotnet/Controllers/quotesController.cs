93c22e950025456722e94b1b0a4eced712f668e6
Gorkem Ozlu <spammailatmayin@gmail.com>
2022-08-10 12:25:56 +0300

    dotnet quote big bang

---
+++ quote-service-dotnet/Controllers/quotesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Controllers
+{
+    [Route("v1")]
+    [ApiController]
+    public class quotesController : ControllerBase
+
+    {
+        // GET api/values/5
+        private static HttpClient Client = new HttpClient();
+        [HttpGet("{quotes}")]
+        public ActionResult<string> Details(string q)
+        {
+            var iexUrl = $"https://sandbox.iexapis.com/stable/stock/{q}/quote?token=Tpk_c05ba4ad3b434f7ab8ffa87cfaab503a";
+            var result = Client.GetAsync(iexUrl);
+            var iexResult = result.Result.Content.ReadAsStringAsync().Result;
+            return iexResult;
+        }
+
+    }
+}
