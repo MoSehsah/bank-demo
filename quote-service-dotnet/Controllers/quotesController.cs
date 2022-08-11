481c09abdc733a2d080e1fd203da8765fc01aa3c
Gorkem Ozlu <spammailatmayin@gmail.com>
2022-08-11 12:24:18 +0300

    update quote-controller

--- quote-service-dotnet/Controllers/quotesController.cs
+++ quote-service-dotnet/Controllers/quotesController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Globalization;
 
 namespace WebApi.Controllers
 {
@@ -23,6 +26,103 @@ namespace WebApi.Controllers
             var iexUrl = $"https://sandbox.iexapis.com/stable/stock/{q}/quote?token=Tpk_c05ba4ad3b434f7ab8ffa87cfaab503a";
             var result = Client.GetAsync(iexUrl);
             var iexResult = result.Result.Content.ReadAsStringAsync().Result;
+            JObject jo = JObject.Parse(iexResult);
+            jo.Add("Status","SUCCESS");
+            jo.Add("Name",jo.Property("companyName").Value);
+            jo.Property("companyName").Remove();
+
+            jo.Add("Symbol",jo.Property("symbol").Value);
+            jo.Property("symbol").Remove();
+
+            jo.Add("LastPrice",jo.Property("latestPrice").Value);
+            jo.Property("latestPrice").Remove();
+
+            jo.Add("Change",jo.Property("change").Value);
+            jo.Property("change").Remove();
+
+            jo.Add("ChangePercent",jo.Property("changePercent").Value);
+            jo.Property("changePercent").Remove();
+
+            var latestUpdateDouble = Convert.ToDouble(jo.Property("latestUpdate").Value);
+            var latestUpdateDate = (DateTimeOffset.UnixEpoch.AddMilliseconds(latestUpdateDouble));
+            
+            jo.Add("Timestamp",latestUpdateDate);
+                
+            //jo.Property("closeTime").Remove();
+            jo.Add("MSDate",null);
+            jo.Add("MarketCap",float.Parse(jo.Property("marketCap").Value.ToString(),CultureInfo.InvariantCulture.NumberFormat));
+            jo.Property("marketCap").Remove();
+
+            jo.Add("Volume",jo.Property("volume").Value);
+            jo.Property("volume").Remove();
+
+            jo.Add("ChangeYTD",null);
+
+            jo.Add("ChangePercentYTD",null);
+
+            jo.Add("High",jo.Property("high").Value);
+            jo.Property("high").Remove();
+
+            jo.Add("Low",jo.Property("low").Value);
+            jo.Property("low").Remove();
+
+            jo.Add("Open",jo.Property("open").Value);
+            jo.Property("open").Remove();
+
+            jo.Add("Currency",jo.Property("currency").Value);
+            jo.Property("currency").Remove();
+
+
+            jo.Property("avgTotalVolume").Remove();
+            jo.Property("calculationPrice").Remove();
+            jo.Property("close").Remove();
+            jo.Property("closeSource").Remove();
+            jo.Property("delayedPrice").Remove();
+            jo.Property("delayedPriceTime").Remove();
+            jo.Property("extendedChange").Remove();
+            jo.Property("extendedChangePercent").Remove();
+            jo.Property("extendedPrice").Remove();
+            jo.Property("highSource").Remove();
+            jo.Property("highTime").Remove();
+            jo.Property("iexAskPrice").Remove();
+            jo.Property("iexAskSize").Remove();
+            jo.Property("iexBidPrice").Remove();
+            jo.Property("iexBidSize").Remove();
+            jo.Property("iexClose").Remove();
+            jo.Property("iexCloseTime").Remove();
+            jo.Property("iexLastUpdated").Remove();
+            jo.Property("iexMarketPercent").Remove();
+            jo.Property("iexOpen").Remove();
+            jo.Property("iexOpenTime").Remove();
+            jo.Property("iexRealtimePrice").Remove();
+            jo.Property("iexRealtimeSize").Remove();
+            jo.Property("iexVolume").Remove();
+            jo.Property("lastTradeTime").Remove();
+            jo.Property("latestSource").Remove();
+            jo.Property("latestTime").Remove();
+            jo.Property("oddLotDelayedPrice").Remove();
+            jo.Property("oddLotDelayedPriceTime").Remove();
+            jo.Property("openSource").Remove();
+            jo.Property("primaryExchange").Remove();
+            jo.Property("ytdChange").Remove();
+            jo.Property("lowSource").Remove();
+            
+
+            jo.Property("extendedPriceTime").Remove();
+            jo.Property("latestUpdate").Remove();
+            jo.Property("latestVolume").Remove();
+            jo.Property("lowTime").Remove();
+            jo.Property("openTime").Remove();
+
+            jo.Property("previousClose").Remove();
+            jo.Property("previousVolume").Remove();
+            jo.Property("peRatio").Remove();
+
+            jo.Property("week52High").Remove();
+            jo.Property("week52Low").Remove();
+            jo.Property("isUSMarketOpen").Remove();
+            //iexResult = jo.ToString();
+            iexResult = JsonConvert.SerializeObject(jo);
             return iexResult;
         }
 
