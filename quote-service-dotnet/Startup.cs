34846910d4190843d0ec21ec281d82b921900497
Mohamed Sehsah <mo.sehsah@gmail.com>
2023-04-05 10:51:34 +0200

    fix date format and values

--- quote-service-dotnet/Startup.cs
+++ quote-service-dotnet/Startup.cs
@@ -25,7 +25,12 @@ namespace WebApi
         {
             services.AddCors();
             services.AddDiscoveryClient(Configuration);
-            services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+            services.AddControllers().AddNewtonsoftJson(options => { 
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                options.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+                options.SerializerSettings.DateFormatString = "ddd MMM dd HH:mm:ss UTCZ yyyy";
+
+                });
             services.AddAllActuators(Configuration);
             services.AddPrometheusActuatorServices(Configuration);
             services.AddDistributedTracingAspNetCore();
@@ -42,7 +47,7 @@ namespace WebApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "quote-service-dotnet", Version = "v1" });
             });
-            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- quote-service-dotnet/src/Models/QuoteMapper.cs
+++ quote-service-dotnet/src/Models/QuoteMapper.cs
@@ -1,35 +1,45 @@
 
 using System;
 
-public class QuoteMapper {
+public class QuoteMapper
+{
     public static QuoteMapper INSTANCE = new QuoteMapper();
+    private static readonly DateTime UnixEpoch =
+    new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-    private QuoteMapper() :base(){
+    private QuoteMapper() : base()
+    {
     }
 
-    public Quote mapFromIexQuote(IexQuote iexQuote) {
-        if (iexQuote == null) {
+    public Quote mapFromIexQuote(IexQuote iexQuote)
+    {
+        if (iexQuote == null)
+        {
             return null;
         }
 
         Quote mappedQuote = new Quote();
-        mappedQuote.Symbol =iexQuote.symbol;
+        mappedQuote.Symbol = iexQuote.symbol;
         mappedQuote.Name = iexQuote.companyName;
         mappedQuote.Open = iexQuote.open;
         mappedQuote.High = iexQuote.high;
         mappedQuote.Low = iexQuote.low;
         mappedQuote.Change = iexQuote.change;
-        mappedQuote.ChangePercent = (float) iexQuote.changePercent;
-        mappedQuote.MarketCap = (float) iexQuote.marketCap;
-        if ("Previous close".Equals(iexQuote.latestSource)) {
+        mappedQuote.ChangePercent = (float)iexQuote.changePercent;
+        mappedQuote.MarketCap = (float)iexQuote.marketCap;
+        if ("Previous close".Equals(iexQuote.latestSource))
+        {
             mappedQuote.LastPrice = iexQuote.close;
-            mappedQuote.Timestamp = new DateTime(iexQuote.closeTime);
-        } else {
+            mappedQuote.Timestamp = UnixEpoch.AddMilliseconds(iexQuote.closeTime);
+
+        }
+        else
+        {
             mappedQuote.LastPrice = iexQuote.latestPrice;
-            mappedQuote.Timestamp = new DateTime(iexQuote.latestUpdate);
+            mappedQuote.Timestamp = UnixEpoch.AddMilliseconds(iexQuote.latestUpdate);
         }
         mappedQuote.Status = "SUCCESS";
-        mappedQuote.Volume = (int) Math.Round((double) iexQuote.avgTotalVolume);
+        mappedQuote.Volume = (int)Math.Round((double)iexQuote.avgTotalVolume);
 
         return mappedQuote;
     }
