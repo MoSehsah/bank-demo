7a645f3e3fd1568ba3c3ee676046cd1e72119795
Gorkem Ozlu <spammailatmayin@gmail.com>
2023-04-09 11:49:05 +0300

    revert

--- quote-service-dotnet/Program.cs
+++ quote-service-dotnet/Program.cs
@@ -8,38 +8,14 @@ namespace WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                   .ConfigureServices(services =>
-                   {
-                       services.AddControllers();
-                       services.AddOpenTelemetryTracing(builder =>
-                       {
-                           builder
-                               .AddAspNetCoreInstrumentation()
-                               .AddHttpClientInstrumentation()
-                               .AddEurekaExporter()
-                               .AddConsoleExporter()
-                               .AddJaegerExporter()
-                               .AddAllActuators()
-                               .AddOtlpExporter(options =>
-                               {
-                                   options.Endpoint = new System.Uri(Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT") ?? "http://jaeger-jaeger-collector.monitoring:4317");
-                               })
-                               .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("quote-service-dotnet"))
-                               .Build();
-                       });
-                   })
-                   .ConfigureWebHostDefaults(webBuilder =>
-                   {
-                       webBuilder.UseStartup<Startup>();
-                   });
-                //.UseStartup<Startup>()
-                //.AddWavefrontMetrics()
-                //.AddDistributedTracingAspNetCore()
-                //.AddAllActuators();
+                .UseStartup<Startup>()
+                .AddWavefrontMetrics()
+                .AddDistributedTracingAspNetCore()
+                .AddAllActuators();
     }
 }
