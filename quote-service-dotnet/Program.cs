a7b2127ea66b787564dd89ecc864c97fc06335ec
Gorkem Ozlu <spammailatmayin@gmail.com>
2023-04-09 13:29:28 +0300

    up

--- quote-service-dotnet/Program.cs
+++ quote-service-dotnet/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Steeltoe.Management.Endpoint;
 using Steeltoe.Management.Tracing;
+using Microsoft.Extensions.DependencyInjection;
 namespace WebApi
 {
     public class Program
@@ -14,6 +15,18 @@ namespace WebApi
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureServices(services =>
+                {
+                    // Enables and automatically starts the instrumentation!
+                    services.AddOpenTracing(builder =>
+                    {
+                        builder.ConfigureAspNetCore(options =>
+                        {
+                            // We don't need any tracing data for our health endpoint.
+                            options.Hosting.IgnorePatterns.Add(ctx => ctx.Request.Path == "/health");
+                        });
+                    });
+                })
                 //.AddWavefrontMetrics()
                 //.AddDistributedTracingAspNetCore()
                 .AddAllActuators();
